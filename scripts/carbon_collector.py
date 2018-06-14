48059a5d3691fd35ae00549a0b84ab9d4cfe880d
Jordan Palacios <jordan.palacios@pal-robotics.com>
2018-06-14 12:50:03 +0200

    Added dry-run option

--- scripts/carbon_collector.py
+++ scripts/carbon_collector.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-import rospy
 import graphitesend
+import rospy
+import sys
 from pal_statistics_msgs.msg import Statistics
 
 class CarbonCollector:
-    def __init__(self, topics):
-        self.gs = graphitesend.GraphiteClient(prefix='', graphite_server='localhost')
+    def __init__(self, topics, dry_run):
+        self.dry_run = dry_run
+        self.gs = graphitesend.GraphiteClient(prefix='', graphite_server='localhost', dryrun=dry_run)
 
         self.statistics_subs = []
         for topic in topics:
@@ -20,14 +22,27 @@ class CarbonCollector:
         for stat in msg.statistics:
             stats.append((stat.name, stat.value))
 
-        self.gs.send_list(stats, timestamp.to_sec())
+        result = self.gs.send_list(stats, timestamp.to_sec())
+
+        if self.dry_run:
+            print result
 
 
 if __name__ == "__main__":
-    rospy.init_node("carbon_collector")
+    rospy.init_node('carbon_collector')
+
+    if not rospy.has_param('~topics'):
+        print "No topics were specified"
+        sys.exit(1)
 
     topics = rospy.get_param('~topics')
 
-    carbon_collector = CarbonCollector(topics)
+    dry_run = False
+    if rospy.has_param('~dry_run'):
+        dry_run = rospy.get_param('~dry_run')
+
+    carbon_collector = CarbonCollector(topics, dry_run=dry_run)
 
     rospy.spin()
+
+    sys.exit(0)
