63fa394995a5a636c2aab5596b0350e549f79470
Jordan Palacios <jordan.palacios@pal-robotics.com>
2018-06-15 10:54:19 +0200

    Moved carbon_collector to its own python package

--- src/pal_carbon_collector/carbon_collector.py
+++ src/pal_carbon_collector/carbon_collector.py
@@ -2,7 +2,6 @@
 
 import graphitesend
 import rospy
-import sys
 from pal_statistics_msgs.msg import Statistics
 
 class CarbonCollector:
@@ -27,22 +26,3 @@ class CarbonCollector:
         if self.dry_run:
             print result
 
-
-if __name__ == "__main__":
-    rospy.init_node('carbon_collector')
-
-    if not rospy.has_param('~topics'):
-        print "No topics were specified"
-        sys.exit(1)
-
-    topics = rospy.get_param('~topics')
-
-    dry_run = False
-    if rospy.has_param('~dry_run'):
-        dry_run = rospy.get_param('~dry_run')
-
-    carbon_collector = CarbonCollector(topics, dry_run=dry_run)
-
-    rospy.spin()
-
-    sys.exit(0)
