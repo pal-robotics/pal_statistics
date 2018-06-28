c5793e5be2fe230ab057781f62523d906341ef13
Jordan Palacios <jordan.palacios@pal-robotics.com>
2018-06-28 11:27:53 +0200

    Moved statsd_collector to its own python package

---
+++ src/pal_statsd_collector/statsd_collector.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import pystatsd
+import rospy
+import socket
+from pal_statistics_msgs.msg import Statistics
+
+class StatsdCollector:
+    def __init__(self):
+        self.fqdn = socket.getfqdn()
+        self.pc = pystatsd.Client(prefix=self.fqdn)
+
+        self.statistics_sub = rospy.Subscriber('/pal_statistics', Statistics, self.statistics_callback)
+
+        self.stats_cfg = { 'pb18.temperature' : ['t'], 'pb17.temperature' : ['t'], 'pb17.charge' : ['g'] }
+
+    def statistics_callback(self, msg):
+        for stat in msg.statistics:
+            stat_type = self.stats_cfg.get(stat.name)
+            if stat_type != None:
+                if 'c' in stat_type:
+                    self.pc.increment(stat.name, stat.value)
+                if 'g' in stat_type:
+                    self.pc.gauge(stat.name, stat.value)
+                if 't' in stat_type:
+                    self.pc.timing(stat.name, stat.value)
+
