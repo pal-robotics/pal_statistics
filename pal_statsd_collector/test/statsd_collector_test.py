3376f96dd864d52aeb8d462029e9d17652f562ad
Jordan Palacios <jordan.palacios@pal-robotics.com>
2018-07-02 15:04:03 +0200

    Added test for statistics callback

--- pal_statsd_collector/test/statsd_collector_test.py
+++ pal_statsd_collector/test/statsd_collector_test.py
@@ -2,6 +2,8 @@
 
 import rospy
 import unittest
+from pal_statistics_msgs.msg import Statistic
+from pal_statistics_msgs.msg import Statistics
 from pal_statsd_collector.statsd_collector import StatsdCollector
 
 SYSTEM_NAME = 'test_system'
@@ -9,7 +11,13 @@ SYSTEM_NAME = 'test_system'
 class StatsdCollectorTest(unittest.TestCase):
 
     def setUp(self):
-        self.sc = StatsdCollector(topics=[])
+        self.topics = [{ 'name': 'test_topic', \
+                        'stats': [ \
+                                  { 'name': 'stat_1', 'type': ['c'] }, \
+                                  { 'name': 'stat_2', 'type': ['g'] }, \
+                                  { 'name': 'stat_3', 'type': ['t'] } \
+                                  ]}]
+        self.sc = StatsdCollector(topics=self.topics)
 
     def test_stat_match(self):
         self.assertTrue( self.sc.stat_match('stat.status', 'stat.status') )
@@ -25,6 +33,16 @@ class StatsdCollectorTest(unittest.TestCase):
         self.assertTrue( self.sc.stat_match('stat.pre.status_3', 'stat.pre.*') )
 
         self.assertTrue( self.sc.stat_match('stat.status', '.*.status') )
+
+    def test_statistics_callback(self):
+        msg = Statistics()
+        msg.statistics.append(Statistic('stat_1', 1.0))
+        msg.statistics.append(Statistic('stat_2', 2.0))
+        msg.statistics.append(Statistic('stat_3', 3.0))
+        try:
+            self.sc.statistics_callback(msg, self.topics[0]['stats'])
+        except:
+            self.fail("Exception raised!")
 
 if __name__ == '__main__':
     import rosunit
