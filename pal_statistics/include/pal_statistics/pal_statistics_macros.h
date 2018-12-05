d1a004e9062529ba1b297342c996d49acb6a9178
Victor Lopez <victor.lopez@pal-robotics.com>
2018-12-05 12:37:41 +0100

    Add macros with variable argument count

--- pal_statistics/include/pal_statistics/pal_statistics_macros.h
+++ pal_statistics/include/pal_statistics/pal_statistics_macros.h
@@ -17,15 +17,28 @@ namespace pal_statistics
 boost::shared_ptr<StatisticsRegistry> getRegistry(const std::string &topic);
 } //namespace pal_statistics
 
-#define REGISTER_VARIABLE(TOPIC, ID, VARIABLE, BOOKKEEPING)                              \
+
+//Trick to use macros with optional argument, in practice there are three version of the macro:
+//REGISTER_VARIABLE(TOPIC, ID, VARIABLE, BOOKKEEPING) -> full specification of arguments
+//REGISTER_VARIABLE(TOPIC, ID, VARIABLE)              -> No bookkeeping
+
+//https://stackoverflow.com/questions/3046889/optional-parameters-with-c-macros?
+#define REGISTER_VARIABLE_3_ARGS(TOPIC, ID, VARIABLE)                              \
+  customRegister(*pal_statistics::getRegistry(TOPIC), ID, VARIABLE);
+#define REGISTER_VARIABLE_4_ARGS(TOPIC, ID, VARIABLE, BOOKKEEPING)                              \
   customRegister(*pal_statistics::getRegistry(TOPIC), ID, VARIABLE, BOOKKEEPING);
+
+#define GET_5TH_ARG(arg1, arg2, arg3, arg4, arg5, ...) arg5
+#define REGISTER_MACRO_CHOOSER(...) \
+    GET_5TH_ARG(__VA_ARGS__, REGISTER_VARIABLE_4_ARGS, \
+                REGISTER_VARIABLE_3_ARGS)
+
+#define REGISTER_VARIABLE(...) REGISTER_MACRO_CHOOSER(__VA_ARGS__)(__VA_ARGS__)
 
 // Register the variable with the same name as the variable name
 #define REGISTER_VARIABLE_SIMPLE(TOPIC, VARIABLE, BOOKKEEPING)                              \
   customRegister(*pal_statistics::getRegistry(TOPIC), #VARIABLE, VARIABLE, BOOKKEEPING);
 
-#define UNREGISTER_VARIABLE(TOPIC, ID_OR_NAME, BOOKKEEPING)                              \
-  pal_statistics::getRegistry(TOPIC)->unregisterVariable(ID_OR_NAME, BOOKKEEPING);
 
 #define PUBLISH_STATISTICS(TOPIC) pal_statistics::getRegistry(TOPIC)->publish();
 
@@ -37,6 +50,20 @@ boost::shared_ptr<StatisticsRegistry> getRegistry(const std::string &topic);
 
 #define PUBLISH_CUSTOM_STATISTICS_MSG(TOPIC, MSG) pal_statistics::getRegistry(TOPIC)->publishCustomStatistics(MSG)
 
+
+#define UNREGISTER_VARIABLE_2_ARGS(TOPIC, ID)                                            \
+  pal_statistics::getRegistry(TOPIC)->unregisterVariable(ID);
+#define UNREGISTER_VARIABLE_3_ARGS(TOPIC, ID, BOOKKEEPING)                               \
+  pal_statistics::getRegistry(TOPIC)->unregisterVariable(ID, BOOKKEEPING);
+
+#define GET_4TH_ARG(arg1, arg2, arg3, arg4, ...) arg4
+#define UNREGISTER_MACRO_CHOOSER(...) \
+    GET_4TH_ARG(__VA_ARGS__, UNREGISTER_VARIABLE_3_ARGS, \
+                UNREGISTER_VARIABLE_2_ARGS)
+
+//UNREGISTER_VARIABLE(TOPIC, ID, BOOKKEEPING) -> full specification of arguments
+//UNREGISTER_VARIABLE(TOPIC, ID)              -> No bookkeeping
+#define UNREGISTER_VARIABLE(...) UNREGISTER_MACRO_CHOOSER(__VA_ARGS__)(__VA_ARGS__)
 
 
 #endif  // PAL_STATISTICS_MACROS_H
