a2d16499ef0bd59a56b75aaa9f7cf7daee4533ca
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2024-11-30 00:58:41 +0100

    Catch exceptions thrown in the publisher thread

--- pal_statistics/src/pal_statistics.cpp
+++ pal_statistics/src/pal_statistics.cpp
@@ -333,22 +333,26 @@ void StatisticsRegistry::publisherThreadCycle()
 {
   rclcpp::WallRate rate(2000);
   while (rclcpp::ok() && !interrupt_thread_) {
-    while (!is_data_ready_ && !interrupt_thread_) {
-      rate.sleep();
+    try {
+      while (!is_data_ready_ && !interrupt_thread_) {
+        rate.sleep();
+      }
+
+      std::unique_lock<std::mutex> data_lock(data_mutex_);
+
+      while (registration_list_->hasPendingData()) {
+        bool minor_changes = updateMsg(names_msg_, values_msg_, true);
+
+        std::unique_lock<std::mutex> pub_lock(pub_mutex_);
+        data_lock.unlock();
+        doPublish(!minor_changes);
+        pub_lock.unlock();
+        data_lock.lock();
+      }
+      is_data_ready_ = false;
+    } catch (const std::exception & e) {
+      RCLCPP_ERROR(getLogger(), "Exception in publisher thread: %s!. Aborting!", e.what());
     }
-
-    std::unique_lock<std::mutex> data_lock(data_mutex_);
-
-    while (registration_list_->hasPendingData()) {
-      bool minor_changes = updateMsg(names_msg_, values_msg_, true);
-
-      std::unique_lock<std::mutex> pub_lock(pub_mutex_);
-      data_lock.unlock();
-      doPublish(!minor_changes);
-      pub_lock.unlock();
-      data_lock.lock();
-    }
-    is_data_ready_ = false;
   }
 }
 
